cb155f45996d80180f74c7aab9137b6a79f0c4cb
wzd5230 <772461552@qq.com>
2016-07-14 00:25:40 +0800

    springMVC using no annotate

---
+++ src/wzd/pkg/ssm/controller/ItemsController1.java
@@ -0,0 +1,30 @@
+package wzd.pkg.ssm.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class ItemsController1 implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+
+		//调用service查找数据库，查询商品列表。
+		
+		
+		
+		//返回ModelAndView
+		ModelAndView modelAndView=new ModelAndView();
+		
+		//相当于request的setAttribute，在jsp页面中通过itemsList取数据
+		modelAndView.addObject("itemsList", "controller1 content");
+		
+		modelAndView.setViewName("/WEB-INF/jsp/items.jsp");
+		
+		return modelAndView;
+	}
+
+}
---
+++ src/wzd/pkg/ssm/controller/ItemsController3.java
@@ -0,0 +1,28 @@
+package wzd.pkg.ssm.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.HttpRequestHandler;
+
+public class ItemsController3 implements HttpRequestHandler {
+
+	@Override
+	public void handleRequest(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		request.setAttribute("itemsList", "controller3 content");
+		
+		
+		request.getRequestDispatcher("/WEB-INF/jsp/items.jsp").forward(request, response);
+		
+		
+	}
+
+
+
+}
